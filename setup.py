0045662b48a13ae633c76192288e164e83d3caed
hzwer <598460606@163.com>
2015-12-26 20:27:23 +0800

    Color outout and remove bs4
    Compatibility

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@
 
 from setuptools import setup, find_packages
 
-version = '0.0.2'
+version = '0.0.4'
 
 setup(name = 'termdic',
       version = version,
       author = 'hzwer',
       author_mail = '598460606@163.com',
       url = 'https://github.com/hzwer/termdic',
-      description = '终端下英文字典',
-      keywords = 'dictionary in terminal',
+      description = 'Dictionary in your terminal',
+      keywords = 'dictionary, terminal',
       packages = find_packages(),
       include_package_data = True,
       zip_safe = False,
       install_requires = [
-          'BeautifulSoup4',
           'requests',
+          'termcolor',
       ],
       entry_points={
         'console_scripts':[
--- termdic/termdic.py
+++ termdic/termdic.py
@@ -3,35 +3,36 @@
 import re
 import sys
 import requests
-from bs4 import BeautifulSoup as bs
 from termcolor import colored 
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
 def look_up(word):
     url = 'http://dict.youdao.com/search?q='
     r = requests.get(url + word)
     content = r.text
-    soup = bs(content, "lxml")
 
     try:
-        ps = soup.find('div', {'class': 'baav'}).find_all('span', {'class': 'phonetic'})
-        mean = soup.find('div', {'class': 'trans-container'}).find('ul')
+        ps = re.findall(re.compile('<span class="phonetic">(.*?)</span>'), content)
+        tmp = re.findall(re.compile('<div class="trans-container">(.*?)</div>', re.S), content)
+        mean = re.findall(re.compile('<li>(.*?)</li>'), tmp[0])
     except:
         print('Can\'t find it')
         return
     
     if len(ps) is 2:
-        print(colored('英{} 美{}'.format(ps[0].get_text().decode('GBK'), ps[1].get_text().decode('GBK')), 'cyan'))
+        try:
+            print(colored('英{} 美{}'.format(ps[0], ps[1]), 'cyan'))
+        except:
+            pass
     else:
         try:
-            print(colored(ps[0].get_text().decode()), 'cyan')
+            print(colored(ps[0]), 'cyan')
         except:
             pass
     
-    tmp = re.split("\n", mean.decode().strip())
-    for text in tmp:
-        print(colored(text, 'cyan'))
+    for line in mean:
+        words = line.split('.', 1)
+        words[0] += '.'
+        print('{}{}'.format(colored(words[0], 'green'), colored(words[1], 'blue')))
 
 def main():
     args = sys.argv[1:]
