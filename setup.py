848864eec22087af785a7b42b86355fb9dc131c2
hzwer <598460606@163.com>
2015-12-27 19:50:37 +0800

    pep8

--- setup.py
+++ setup.py
@@ -4,24 +4,24 @@ from setuptools import setup, find_packages
 
 version = '0.0.4'
 
-setup(name = 'termdic',
-      version = version,
-      author = 'hzwer',
-      author_mail = '598460606@163.com',
-      url = 'https://github.com/hzwer/termdic',
-      description = 'Dictionary in your terminal',
-      keywords = 'dictionary, terminal',
-      packages = find_packages(),
-      include_package_data = True,
-      zip_safe = False,
-      install_requires = [
-          'requests',
-          'termcolor',
-      ],
-      entry_points={
-        'console_scripts':[
+setup(
+    name = 'termdic',
+    version = version,
+    author = 'hzwer',
+    author_mail = '598460606@163.com',
+    url = 'https://github.com/hzwer/termdic',
+    description = 'Dictionary in your terminal',
+    keywords = 'dictionary, terminal',
+    packages = find_packages(),
+    include_package_data = True,
+    zip_safe = False,
+    install_requires = [
+        'requests',
+        'termcolor',
+    ],
+    entry_points = {
+        'console_scripts': [
             'tw = termdic.termdic:main'
         ]
-      },     
+    },
 )
-      
--- termdic/termdic.py
+++ termdic/termdic.py
@@ -3,7 +3,8 @@
 import re
 import sys
 import requests
-from termcolor import colored 
+from termcolor import colored as cl
+
 
 def look_up(word):
     url = 'http://dict.youdao.com/search?q='
@@ -11,30 +12,31 @@ def look_up(word):
     content = r.text
 
     try:
-        ps = re.findall(re.compile('<span class="phonetic">(.*?)</span>'), content)
-        tmp = re.findall(re.compile('<div class="trans-container">(.*?)</div>', re.S), content)
+        ps = re.findall(re.compile('"phonetic">(.*?)</span>'), content)
+        pattern = re.compile('"trans-container">(.*?)</div>', re.S)
+        tmp = re.findall(pattern, content)
         mean = re.findall(re.compile('<li>(.*?)</li>'), tmp[0])
     except:
         print('Can\'t find it')
         return
-    
+
     if len(ps) is 2:
-            print(colored(u'英{0} 美{1}'.format(ps[0], ps[1]), 'cyan'))
+            print(cl(u'英{0} 美{1}'.format(ps[0], ps[1]), 'cyan'))
     else:
         try:
-            print(colored(ps[0]), 'cyan')
+            print(cl(ps[0]), 'cyan')
         except:
             pass
-    
     for line in mean:
         words = line.split('.', 1)
         if len(words) is 2:
             words[0] += '.'
-            print(u'{0}{1}'.format(colored(words[0], 'green'), colored(words[1], 'blue')))
+            print(u'{0}{1}'.format(cl(words[0], 'green'), cl(words[1], 'blue')))
             # word
         else:
-            print(u'{0}'.format(colored(words[0], 'blue')))
+            print(u'{0}'.format(cl(words[0], 'blue')))
             # phrase
+
 
 def main():
     args = sys.argv[1:]
