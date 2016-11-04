d085c8969a8dc956479d517576ea9c46f5601ed4
Tyrion Zhou <wulala979@outlook.com>
2016-11-04 16:29:04 +0800

    rebuild and add terminal model

--- termdic/termdic.py
+++ termdic/termdic.py
@@ -6,79 +6,106 @@ import sys
 import requests
 from termcolor import colored as cl
 
-def look_up(word):
-    url = 'http://dict.youdao.com/search?q='
-    r = requests.get(url + word)
-    content = r.text
 
-    try:
-        ps = re.findall(re.compile('"phonetic">(.*?)</span>'), content)
-        pattern = re.compile('"trans-container">(.*?)</div>', re.S)
-        tmp = re.findall(pattern, content)
-        mean = re.findall(re.compile('<li>(.*?)</li>'), tmp[0])
-    except:
-        print('Can\'t find it')
-        return
+class queryInfo:
+	speak = False
 
-    if len(ps) is 2:
-            print(cl(u'英{0} 美{1}'.format(ps[0], ps[1]), 'cyan'))
-    else:
-        try:
-            print(cl(ps[0], 'cyan'))
-        except:
-            pass
-    for line in mean:
-        words = line.split('.', 1)
-        if len(words) is 2:
-            words[0] += '.'
-            print(u'{0}{1}'.format(cl(words[0], 'green'), cl(words[1], 'blue')))
-            # word
-        else:
-            print(u'{0}'.format(cl(words[0], 'blue')))
-            # phrase
+info = queryInfo
+
+def speakWord(word):
+	if sys.platform == 'darwin':
+		os.system('say ' + str(word))
+	else:
+		os.system('espeak ' + str(word))
+
+def lookUp(word):
+	url = 'http://dict.youdao.com/search?q='
+	try:
+		r = requests.get(url + word)
+	except e:
+		print e
+		return 
+	content = r.text
+
+	try:
+		ps = re.findall(re.compile('"phonetic">(.*?)</span>'), content)
+		pattern = re.compile('"trans-container">(.*?)</div>', re.S)
+		tmp = re.findall(pattern, content)
+		mean = re.findall(re.compile('<li>(.*?)</li>'), tmp[0])
+	except:
+		print('Can\'t find it')
+		return
+
+	if len(ps) is 2:
+			print(cl(u'英{0} 美{1}'.format(ps[0], ps[1]), 'cyan'))
+	else:
+		try:
+			print(cl(ps[0], 'cyan'))
+		except:
+			pass
+	for line in mean:
+		words = line.split('.', 1)
+		if len(words) is 2:
+			words[0] += '.'
+			print(u'{0}{1}'.format(cl(words[0], 'green'), cl(words[1], 'blue')))
+			# word
+		else:
+			print(u'{0}'.format(cl(words[0], 'blue')))
+			# phrase
+
+	if info.speak:
+		speakWord(word)
 
 
-def print_version():
-    from termdic import __version__
-    print('termdic v{}'.format(__version__))
-    sys.exit(0)
+def printVersion():
+	from termdic import __version__
+	print('termdic v{}'.format(__version__))
+	sys.exit(0)
 
 
-def print_usage():
-    print('usage: termdic [-v] [word] [word -p]')
-    sys.exit(0)
+def printUsage():
+	print('usage: termdic [-v] [word] [word -p]')
+	sys.exit(0)
+
+def terminalModel():
+	while True:
+		print ">>> ", 
+		temp = sys.stdin.readline().strip('\n').split()
+		if len(temp) > 1:
+			print "Syntax Error!"
+			print "Use :q to exit."
+			continue
+		elif len(temp) == 0: 
+			continue
+		
+		word = temp[0]
+		if word == ":q":
+			return 
+		else:
+			lookUp(word)
 
 
 def main():
-    args = sys.argv[1:]
-    word = " ".join(args)
-    if len(args) == 0:
-        print_usage()
-    opt_pos = -1
-    if args[0][0] == '-':
-        opt_pos = 0
-    args[opt_pos] = args[opt_pos].strip()
-    if args[opt_pos][0] is '-':
-        if args[opt_pos] == '-v' or args[opt_pos] == '--version':
-            print_version()
-        elif args[opt_pos] == '-p':
-            if opt_pos == -1:
-                word = " ".join(args[:-1])
-            else:
-                word = " ".join(args[1:])
-        else:
-            print_usage()
-
-    if word:
-        look_up(word)
-        if args[opt_pos] == '-p':
-            try:
-                if sys.platform == 'darwin':
-                    os.system('say ' + str(word))
-                else:
-                    os.system('espeak ' + str(word))
-            except:
-                pass
+	args = sys.argv[1:]
+	pivot = ""
+	for x in args:
+		if x == '-p':
+			info.speak = True
+		else:
+			if pivot:
+				print "Syntax Error!"
+				print "Use -u to learn the usage."
+				return
+			else:
+				pivot = x
+	if pivot == '-v':
+		printVersion()
+	elif pivot == '-u':
+		printUsage()
+	elif pivot:
+		lookUp(pivot)
+	else:
+		terminalModel()
 
 if __name__ == '__main__':
-    main()
+	main()
